8a2b74e3fcccc68296254f6953305092afb88dc7
srinivasaraomogili007 <119481381+srinivasaraomogili007@users.noreply.github.com>
2022-11-30 13:25:51 +0530

    Update JobController.java

--- spring-boot-jobrunr/src/main/java/com/mkyong/api/JobController.java
+++ spring-boot-jobrunr/src/main/java/com/mkyong/api/JobController.java
@@ -47,10 +47,12 @@ public class JobController {
 
         return "Job is scheduled.";
     }
+	public string updateJobInfo() {
+		return null;
+	}
+	
 	public string fetchJobInfo() {
 		return null;
 	}
-
-
 
 }
